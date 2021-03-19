df5065e1645f74e5bd5a0edb96f7640d70640d89
Shashank <s_shashank@outlook.com>
2021-03-19 21:17:08 +0530

    added redux state for the app

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Navbar, Nav, Button } from "react-bootstrap";
+import { Navbar, Nav } from "react-bootstrap";
 import { LinkContainer } from "react-router-bootstrap";
 
 const Header = () => {
